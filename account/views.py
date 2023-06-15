a54f71029ba0e4623372c4d99fc60593edaa4c4a
Success Divine Onyechinemere <divinesuccess228@gmail.com>
2023-06-15 16:01:06 +0100

    -add and display user actions on the activity screen

--- account/views.py
+++ account/views.py
@@ -10,6 +10,8 @@ from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from .models import Contact
+from actions.utils import create_action
+from actions.models import Action
 
 # Create your views here.
 
@@ -36,7 +38,20 @@ from .models import Contact
 
 @login_required
 def dashboard(request):
-    return render(request, 'account/dashboard.html', {'section':'dashboard'})
+    #display all actions by default
+
+    actions = Action.objects.exclude(user=request.user)
+
+    following_ids = request.user.following.values_list('id', flat=True)
+
+    if following_ids:
+        #if user is following others, retrieve only their actions
+
+        actions = actions.filter(user_id__in=following_ids)
+
+    actions = actions[:10]
+
+    return render(request, 'account/dashboard.html', {'section':'dashboard', 'actions':actions})
     
 
 def register(request):
@@ -54,6 +69,7 @@ def register(request):
             new_user.save()
             #Create the user profile
             Profile.objects.create(user=new_user)
+            create_action(new_user, 'has created an account')
             return render(request, 'account/register_done.html', {'new_user':new_user})
         
     else:
@@ -110,6 +126,7 @@ def user_follow(request):
 
             if action == 'follow':
                 Contact.objects.get_or_create(user_from=request.user, user_to=user)
+                create_action(request.user, 'is following', user)
 
             else:
                 Contact.objects.filter(user_from=request.user, user_to=user).delete()
